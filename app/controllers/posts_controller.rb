5827963ea174909f39a6807cecc84456ef1ead62
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-10-08 23:06:38 +0300

    Create posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect article: [:title, :body]
+  end
+end

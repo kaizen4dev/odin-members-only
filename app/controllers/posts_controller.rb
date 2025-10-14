1964e0d5923b15d017ab293b06dcb419f0c5479f
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-10-14 21:38:50 +0300

    fix: redirect to index, since there is no post route/path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
     @post = Post.new post_params
 
     if @post.save
-      redirect_to @post
+      redirect_to posts_path
     else
       render :new, status: :unprocessable_entity
     end

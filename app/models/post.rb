2a02e169bacc8f0bfc06e2fdf83fa0e27851c0f7
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-10-14 21:14:36 +0300

    Lower character limit for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { in: 3..40 }
-  validates :body, presence: true, length: { maximum: 100000 }
+  validates :body, presence: true, length: { maximum: 1000 }
 end
